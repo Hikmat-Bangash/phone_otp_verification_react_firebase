2abee7e316250ba6b1796b4a95e517b8a415b772
Hikmat-Bangash <hikmatkhanbanagash@gmail.com>
2023-12-28 22:16:13 +0500

    phone otp verficiation code

---
+++ src/App.js
@@ -0,0 +1,106 @@
+
+import { useState } from "react";
+import "react-phone-input-2/lib/style.css";
+import { auth } from "./firebase.config";
+import { RecaptchaVerifier, signInWithPhoneNumber } from "firebase/auth";
+import { toast, Toaster } from "react-hot-toast";
+import OTPVerificationComponent from "./OTPVerificationComponent";
+import PhoneVerificationComponent from "./PhoneVerificationComponent";
+
+const App = () => {
+  const [otp, setOtp] = useState("");
+  const [ph, setPh] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [showOTP, setShowOTP] = useState(false);
+  const [user, setUser] = useState(null);
+
+  function onCaptchVerify() {
+    
+    if (!window.recaptchaVerifier) {
+      window.recaptchaVerifier = new RecaptchaVerifier(
+        "recaptcha-container",
+        {
+          size: "invisible",
+          callback: (response) => {
+            onSignup();
+          },
+          "expired-callback": () => {},
+        },
+        auth
+      );
+    }
+  }
+
+  function onSignup() {
+    setLoading(true);
+    onCaptchVerify();
+
+    const appVerifier = window.recaptchaVerifier;
+
+    const formatPh = "+" + ph;
+
+    signInWithPhoneNumber(auth, formatPh, appVerifier)
+      .then((confirmationResult) => {
+        window.confirmationResult = confirmationResult;
+        setLoading(false);
+        setShowOTP(true);
+        toast.success("OTP sended successfully!");
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoading(false);
+      });
+  }
+
+  function onOTPVerify() {
+    setLoading(true);
+    window.confirmationResult
+      .confirm(otp)
+      .then(async (res) => {
+        console.log(res);
+        setUser(res.user);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+      });
+  }
+
+  return (
+    <section className="bg-emerald-500 flex items-center justify-center h-screen">
+      <div>
+        <Toaster toastOptions={{ duration: 4000 }} />
+        <div id="recaptcha-container"></div>
+        {user ? (
+          <h2 className="text-center text-white font-medium text-2xl">
+            👍Login Success
+          </h2>
+        ) : (
+          <div className="w-80 flex flex-col gap-4 rounded-lg p-4">
+            <h1 className="text-center leading-normal text-white font-medium text-3xl mb-6">
+              Welcome to <br /> CODE A PROGRAM
+            </h1>
+            {showOTP ? (
+              <OTPVerificationComponent
+                otp={otp}
+                setOtp={setOtp}
+                onOTPVerify={onOTPVerify}
+                loading={loading}
+              />
+            ) : (
+              <PhoneVerificationComponent
+                ph={ph}
+                setPh={setPh}
+                onSignup={onSignup}
+                loading={loading}
+              />
+            )}
+          </div>
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default App;
